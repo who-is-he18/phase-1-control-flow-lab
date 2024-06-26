c7286deba8fff3ff738093ef75f8f3d5507e5af9
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-06-26 20:10:15 +0300

    lab.md

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Value){
+    if(Value <= 400){
+        return 'This one is on me!'
+    }else if(Value > 400 && Value< 2000){
+        return 'That will be twenty bucks.'
+    }else if(Value >= 2000 && Value <=2500){
+        return 'I will gladly take your thirty bucks.'
+    }else if(Value > 2500){
+        return 'No can do.'
+    }
+    Value(2501)
 }
+function ternaryCheckCity(city){
+    if(city === 'NYC'){
+        return "Ok, sounds good."
+    }else{
+        return "No go." 
+    }
+}
+function switchOnCharmFromTip(tip){
+    if(tip === 'generous'){
+        return "Thank you so much."
+        } else if(tip === 'not as generous'){
+        return 'Thank you.'
+        }else {
+            return 'Bye.'
+            
+        }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+   
+
+
+
+
+
+
+
